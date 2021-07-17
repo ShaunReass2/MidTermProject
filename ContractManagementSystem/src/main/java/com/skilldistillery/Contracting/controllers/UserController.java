2d3a4682b60ae78a79f048e2021af31591918b92
David <destrada2193@gmail.com>
2021-07-17 16:29:41 -0700

    UserController made

---
+++ ContractManagementSystem/src/main/java/com/skilldistillery/Contracting/controllers/UserController.java
@@ -0,0 +1,35 @@
+package com.skilldistillery.Contracting.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.skilldistillery.Contracting.data.UserDAO;
+import com.skilldistillery.Contracting.entities.User;
+
+@Controller
+public class UserController {
+	
+	@Autowired
+	private UserDAO userDAO;
+	
+	@RequestMapping(path = "adminAccountCreation.do", method = RequestMethod.POST)
+	public String adminAccountCreation(User user, Model model, RedirectAttributes redir) {
+		
+		if (userDAO.findByUsername(user.getUsername())) {
+			model.addAttribute("accountCreationFlag", true);
+			return "error"; // WE NEED THIS JSP !!!
+		}
+		redir.addFlashAttribute("user", userDAO.createUser(user));
+		return "redirect:adminAccountCreated.do";
+	}
+	
+	@RequestMapping(path = "adminAccountCreated.do", method = RequestMethod.GET)
+	public String adminAccountCreated() {
+		return "dashboard"; // WE NEED THIS JSP!!!!!
+	}
+
+}
