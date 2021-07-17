eaf9d730d1f9b3e6a4af2f68ecddf66c5fd11944
Hopeful89 <brandon.stine89@gmail.com>
2021-07-17 17:47:10 -0600

    correcting merge failure

--- ContractManagementSystem/src/main/java/com/skilldistillery/Contracting/data/UserDAOImpl.java
+++ ContractManagementSystem/src/main/java/com/skilldistillery/Contracting/data/UserDAOImpl.java
@@ -1,5 +1,7 @@
 package com.skilldistillery.Contracting.data;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
@@ -15,11 +17,35 @@ public class UserDAOImpl implements UserDAO {
 	@PersistenceContext
 	private EntityManager em; 
 	
+	
 
 	@Override
 	public User findById(int id) {
 		return em.find(User.class, id); 
-	
 	}
 
+	@Override
+	public boolean findByUsername(String username) {
+		
+		String jpql = "SELECT u FROM User u WHERE u.username = :un";
+		List<User> users = em.createQuery(jpql, User.class).setParameter("un", username).getResultList();
+		for (User user : users) {
+			if (user.getUsername().equals(username)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public User createUser(User user) {
+		if (user != null) {
+			try {
+				em.persist(user);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return user;
+	}
 }
