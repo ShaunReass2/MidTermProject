8443a43fdf152100a9bbc12b257a3c2c5d704665
TMShumway <ryujindra@gmail.com>
2021-07-17 15:29:07 -0600

    added new mappings to User, new jsp: AdminAccountCreation

---
+++ ContractManagementSystem/src/main/webapp/WEB-INF/AdminAccountCreation.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+
+<%@ include file="NavBar.jsp" %>
+
+</head>
+<body>
+
+
+
+<!-- Footer JSP Below -->
+<%@ include file="Footer.jsp" %>
+</body>
+</html>
--- JPAContracting/src/main/java/com/skilldistillery/Contracting/entities/User.java
+++ JPAContracting/src/main/java/com/skilldistillery/Contracting/entities/User.java
@@ -2,6 +2,7 @@ package com.skilldistillery.Contracting.entities;
 
 import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -18,6 +19,12 @@ public class User {
 	private String username;
 	
 	private String password;
+	
+	@Column(name="first_name")
+	private String firstName;
+
+	@Column(name="last_name")
+	private String lasttName;
 	
 	private boolean enabled; 
 	
