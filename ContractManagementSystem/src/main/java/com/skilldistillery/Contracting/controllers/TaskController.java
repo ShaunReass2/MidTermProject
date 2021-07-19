e59224add7b8b2f3b5396cb27cfa2f4d9318f370
TMShumway <ryujindra@gmail.com>
2021-07-19 13:50:55 -0600

    added a create task method to task dao and a create task method to task controller

--- ContractManagementSystem/src/main/java/com/skilldistillery/Contracting/controllers/TaskController.java
+++ ContractManagementSystem/src/main/java/com/skilldistillery/Contracting/controllers/TaskController.java
@@ -4,10 +4,12 @@ package com.skilldistillery.Contracting.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.skilldistillery.Contracting.data.TaskDAO;
 import com.skilldistillery.Contracting.data.UserDAO;
+import com.skilldistillery.Contracting.entities.Job;
 import com.skilldistillery.Contracting.entities.Task;
 
 @Controller
@@ -26,6 +28,19 @@ public class TaskController {
 		
 		if(!task.getIsComplete()) {
 			redir.addFlashAttribute("taskCompleteFlag", true);
+			return "redirect:Error.do";
+		}
+		
+		return "redirect:singleJobView.do?id=" + task.getJob().getId();
+	}
+
+	@RequestMapping(path = "addNewTaskToJob.do", method = RequestMethod.POST)
+	public String createTaskByJobId(RedirectAttributes redir, Task task) {
+		
+		Task managedTask = taskDAO.createTask(task);
+		
+		if(managedTask == null) {
+			redir.addFlashAttribute("taskCreationFlag", true);
 			return "redirect:Error.do";
 		}
 		
